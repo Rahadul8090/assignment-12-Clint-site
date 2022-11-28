ffcd3eb6a965bb86621efe84b5eef7e16b2ccbc2
Rahadul8090 <rahadulislam8090@gmail.comm>
2022-11-29 00:36:22 +0600

    add prodect

--- src/Component/Page/AllProdects.js
+++ src/Component/Page/AllProdects.js
@@ -1,52 +1,96 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
+import toast from 'react-hot-toast';
 import { AiOutlinePlus } from "react-icons/ai";
 import { useParams } from 'react-router-dom';
 import { authContext } from '../Auth/Contex';
+import ProdectCard from './ProdectCard';
 
 const Samsung = () => {
     const id = useParams()
     // console.log(id)
     const { user } = useContext(authContext)
-    // const [data, setData] = useState([])
-    // useEffect(() => {
-    //     fetch('http://localhost:5000/categore')
-    //         .then(res => res.json())
-    //         .then(samsung => setData(samsung))
-    // }, [])
+    const [data, setData] = useState([])
+    useEffect(() => {
+        fetch('http://localhost:5000/prodect')
+            .then(res => res.json())
+            .then(samsung => {
+                const prodect = samsung.filter(categorepro => categorepro.brand === id.id)
+                console.log(prodect)
+                 setData(prodect)
+            })
+        }, [id.id])
+    console.log(data)
 
-
-    const FormData = event => {
+    const localData = event => {
         event.preventDefault();
         // console.log(event.target.photoURL.value)
-        const data = event.target
-        const name = data.name.value
-        const userPhoto = data.userPhoto.value
-        const email = data.userEmail.value
-        const brand = data.brand.value
-        const orgPrice = data.orgPrice.value
-        const resellPrice = data.resellPrice.value
-        const imgUrl = data.prodectimg.value
-        const disc = data.disc.value
-        const phoneNumber = data.number.value
-        const location = data.location.value
+        const form = event.target
+        const name = form.name.value
+        const userPhoto = form.userPhoto.value
+        const email = form.userEmail.value
+        const brand = form.brand.value
+        const orgPrice = form.orgPrice.value
+        const resellPrice = form.resellPrice.value
+        const imgUrl = form.prodectimg.value
+        const disc = form.disc.value
+        const phoneNumber = form.number.value
+        const location = form.location.value
 
-        const AttData ={name,phoneNumber,location, userPhoto, email, brand, orgPrice, resellPrice, imgUrl, disc}
-        console.log(AttData)
+
+        const AllData = {
+            name, phoneNumber, location, userPhoto, email,
+            brand, orgPrice, resellPrice, imgUrl, disc
+        }
+
+        fetch('http://localhost:5000/prodect', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(AllData)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                if (data.acknowledged) {
+
+                    toast.success('Add new Product successfully')
+                    form.reset();
+                }
+            })
+            .catch(err => console.error(err))
+        console.log(AllData)
     }
+
+
+
+
+
     return (
         <div>
             <div>
                 <img alt='' className='w-5/6 m-auto  h-96 ' src='https://www.yugatech.com/wp-content/uploads/2022/03/Samsung-Galaxy-M33-5G-Banner-720x270.jpg' />
             </div>
             <div>
-                <div className='shadow-lg shadow-black w-5/6 m-auto text-center'> <label htmlFor="my-modal-4">Add Prodect<AiOutlinePlus className='w-60 h-60 ' /></label></div>
+                <div className='shadow-lg shadow-black w-5/6 m-auto text-center grid grid-cols-3'> 
+                    {
+                    data.map(card => 
+                        <ProdectCard
+                        key={card._id}
+                        prodect={card}
+                        ></ProdectCard>
+                        )
+                   }
+                    </div>
+                    <label htmlFor="my-modal-4">Add Prodect<AiOutlinePlus className='w-60 h-60 ' /></label>
+
                 <input type="checkbox" id="my-modal-4" className="modal-toggle" />
                 <div className="modal">
                     <div className="modal-box relative">
                         <label htmlFor="my-modal-4" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
                         <h3 className="text-lg font-bold"> Rahad Vai</h3>
-                        <form onSubmit={FormData} className='w-96 m-auto'>
+                        <form onSubmit={localData} className='w-96 m-auto'>
                             <input name='name' type="text" placeholder="name" defaultValue={user.displayName} className="input input-bordered m-2 input-info w-full max-w-xs" />
                             <input name='userPhoto' type="text" placeholder="" defaultValue={user.photoURL} className="input input-bordered m-2 input-info w-full max-w-xs" />
                             <input name='userEmail' type="text" placeholder="email" defaultValue={user.email} className="input input-bordered m-2 input-info w-full max-w-xs" />
